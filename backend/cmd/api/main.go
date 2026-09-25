778814542ba38a830e3fc45a04112db07e57e430
Giorgos Tsaloukidis <itsalou23@gmail.com>
2026-09-25 17:08:18 +0300

    connect backend to PostgreSQL

--- backend/cmd/api/main.go
+++ backend/cmd/api/main.go
@@ -1,18 +1,32 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"net/http"
+	"os"
 
 	"github.com/go-chi/chi/v5"
+	"github.com/jackc/pgx/v5"
+	"github.com/joho/godotenv"
 )
 
 func main() {
 	fmt.Println("Warehouse API starting...")
+	godotenv.Load("../.env")
+	dbURL := os.Getenv("DATABASE_URL")
+	conn, err := pgx.Connect(context.Background(), dbURL)
+	if err != nil {
+		fmt.Println("Database connection failed:", err)
+		return
+	}
+	fmt.Println("Database connected")
+	defer conn.Close(context.Background())
 	r := chi.NewRouter()
 	r.Get("/health", func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("OK"))
 	})
+
 	http.ListenAndServe(":8080", r)
 
 }
--- backend/go.mod
+++ backend/go.mod
@@ -2,4 +2,11 @@ module github.com/tsalou23/warehouse
 
 go 1.25.1
 
-require github.com/go-chi/chi/v5 v5.3.2 // indirect
+require (
+	github.com/go-chi/chi/v5 v5.3.2 // indirect
+	github.com/jackc/pgpassfile v1.0.0 // indirect
+	github.com/jackc/pgservicefile v0.0.0-20240606120523-5a60cdf6a761 // indirect
+	github.com/jackc/pgx/v5 v5.11.0 // indirect
+	github.com/joho/godotenv v1.5.1 // indirect
+	golang.org/x/text v0.29.0 // indirect
+)
